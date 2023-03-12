94fdf30b74d02e520cbffc359e3d29556ab81d3a
RicardoJiang <2868405029@qq.com>
2023-03-12 16:25:19 +0800

    ADD: init project

---
+++ app/src/main/java/com/zj/android/performance/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zj.android.performance
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
